3b82ab6cdfa823424e417bf681103df0e1e41ad8
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-07-07 09:04:28 +0530

    U:sugnup page fnc

--- signup/signup.js
+++ signup/signup.js
@@ -1,8 +1,8 @@
 async function submitdata(event){
     event.preventDefault();
-    let name=document.getElementById('password').value;
-    let email=document.getElementById('password').value;
-    let ph=document.getElementById('password').value;
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
+    let ph=document.getElementById('no').value;
     let password=document.getElementById('password').value;
     let confirm=document.getElementById('confirm').value;
     if(password!=confirm){
@@ -15,13 +15,20 @@ async function submitdata(event){
             ph:ph,
             password:password
         }
-        let dataCreation = await axios.post('http://localhost:3000/createuser',details);
-        if(dataCreation.request.status===409){
-            alert('user already exist');
-        }else if(dataCreation.request.status===200){
-            alert('user creates successfully');
-        }else{
-            alert('something wrong');
-        }
+        console.log(details);
+        let dataCreation = await axios.post('http://localhost:3000/signup/createuser',details).then(result=>{
+            console.log(result)
+            if(result.request.status===200){
+                alert("user created successfully")
+            }
+        }).catch((err)=>{
+            console.log(err);
+            if(err.response.request.status===409){
+                alert('user already exixt')
+            }else{
+                alert('something wrong');
+            }
+        })
+        
     }
-}
+};
