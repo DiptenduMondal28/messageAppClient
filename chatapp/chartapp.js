b2aa6986f49702b5c60da5cfd3f759c4cb96917f
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-07-08 09:45:46 +0530

    show mesage

--- chatapp/chartapp.js
+++ chatapp/chartapp.js
@@ -1,7 +1,7 @@
 const messageButton = document.getElementById('messageButton');
 
 messageButton.addEventListener('click',async(e)=>{
-    e.preventDefault();
+    // e.preventDefault();
     const message=document.getElementById('message').value;
     console.log(message)
     const token = localStorage.getItem('token')
@@ -12,3 +12,27 @@ messageButton.addEventListener('click',async(e)=>{
     })
 });
 
+document.addEventListener('DOMContentLoaded',async(e)=>{
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+    await axios.get('http://localhost:3000/user/reply',{headers:{'Authorization':token}}).then(response=>{
+        // const messageBox=document.getElementById('message-container');
+        // const reply = document.createElement('')
+        console.log(response.data.message);
+        for(const reply of response.data.message){
+            show(reply);
+        }
+    })
+})
+
+async function show(reply){
+    const messageBox=document.getElementById('message-container');
+    const mesageshow = document.createElement('div');
+    mesageshow.innerHTML=`<h4>UserID:${reply.userId  }message:${reply.message}</h4>`
+    messageBox.appendChild(mesageshow);
+}
+
+setTimeout(function() {
+    location.reload();
+  }, 50000); 
+// setInterval('show',500)
