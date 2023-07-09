8e01fdc4ffeaf30b581de1da0b32406b36da925f
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-07-09 10:13:00 +0530

    messages save in local storage

--- chatapp/chartapp.js
+++ chatapp/chartapp.js
@@ -16,17 +16,44 @@ messageButton.addEventListener('click',async(e)=>{
 document.addEventListener('DOMContentLoaded',async(e)=>{
     e.preventDefault();
     const token = localStorage.getItem('token');
-    setInterval(async()=>{
-        await axios.get('http://localhost:3000/user/reply',{headers:{'Authorization':token}}).then(response=>{
-        console.log(response.data.message);
-        for(const reply of response.data.message){
-            show(reply);
+    const local= localStorage.getItem('messages')
+    let start=0;
+    console.log(local)
+    if(!local){
+        start=0;
+    }else{
+        let messageArrray=JSON.parse(local);
+        let len=messageArrray.length;
+        start = messageArrray[len-1].id;
+
+    }
+    
+    console.log(start);
+    // setInterval(async()=>{
+        await axios.get(`http://localhost:3000/user/reply?start=${start}`,{headers:{'Authorization':token}}).then(response=>{
+            console.log(response.data.message);
+            const backData=response.data.message;
+            let localMessage=localStorage.getItem('messages');
+            if(!localMessage){
+                localStorage.setItem('messages',JSON.stringify(backData));
+            }else{
+                const arrayOflocalMessage=JSON.parse(localMessage);
+                const totalMessges=arrayOflocalMessage.concat(backData);
+                localStorage.setItem('messages',JSON.stringify(totalMessges));
+            }
+            const replies = JSON.parse(localStorage.getItem('messages'));
+            
+            for(reply of replies){
+                show(reply);
         }
+        
         });
 
-    },1000)
+    // },1000)
 })
 
+
+  
 
 async function show(reply){
     const messageBox=document.getElementById('message-container');
@@ -35,8 +62,8 @@ async function show(reply){
     const { width } = mesageshow.getBoundingClientRect();
     mesageshow.style.width = width + 'px';
     messageBox.appendChild(mesageshow);
-    setInterval(async()=>{
-        mesageshow.innerHTML='';
-    },950);
+    // setInterval(async()=>{
+    //     mesageshow.innerHTML='';
+    // },950);
 }
 
