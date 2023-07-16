0a1d61ee73e86b3369defb725d12321ac672fef3
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-07-16 12:13:19 +0530

    fixed bug for image upload

--- chatapp/chartapp.js
+++ chatapp/chartapp.js
@@ -82,25 +82,52 @@ async function findGroup(group){
 
 //send file to the group
 async function filesend(groupid){
-    const sendAttachmentButton=document.getElementById('sendAttachmentButton');
-    sendAttachmentButton.addEventListener('click',async(e)=>{
+    // const sendAttachmentButton=document.getElementById('sendAttachmentButton');
+    // sendAttachmentButton.addEventListener('click',async(e)=>{
+    //     e.preventDefault();
+    //     const attachment=document.getElementById('sendAttachment');
+    //     const files=attachment.files[0];
+
+    //     const file = new FormData();
+    //     file.append('file', files);
+
+    //     console.log(file);
+    //     const token=localStorage.getItem('token');
+    //     await axios.post('http://localhost:3000/user/file',{file:file,groupid:groupid},{headers:{
+    //         'Authorization':token,
+    //         'Content-Type': 'multipart/form-data'
+    //     }}).then(res=>{
+    //         console.log(res);
+    //     }).catch(err=>{
+    //         console.log(err);
+    //     })
+    // });
+    const sendAttachmentButton = document.getElementById('sendAttachmentButton');
+    sendAttachmentButton.addEventListener('click', async (e) => {
         e.preventDefault();
-        const attachment=document.getElementById('sendAttachment');
-        const files=attachment.files[0];
+        const attachment = document.getElementById('file');
+        const file = attachment.files[0];
 
-        const file = new FormData();
-        file.append('file', files);
+        if (file) {
+        const token = localStorage.getItem('token');
+        const formData = new FormData();
+        formData.append('file', file);
+        console.log(formData)
 
-        console.log(file);
-        const token=localStorage.getItem('token');
-        await axios.post('http://localhost:3000/user/file',{file:file,groupid:groupid},{headers:{
-            'Authorization':token,
-            'Content-Type': 'multipart/form-data'
-        }}).then(res=>{
+        await axios
+            .post(`http://localhost:3000/user/file?groupid=${groupid}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                Authorization: token,
+            },
+            })
+            .then((res) => {
             console.log(res);
-        }).catch(err=>{
+            })
+            .catch((err) => {
             console.log(err);
-        })
+            });
+        }
     });
 }
 
